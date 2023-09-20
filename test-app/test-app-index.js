b9cf28f5ecc4e5e690e5a662211426886c3f7dc2
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2023-09-20 11:55:51 +0200

    Some test cases added

--- test-app/test-app-index.js
+++ test-app/test-app-index.js
@@ -1,25 +1,52 @@
 import { SwedishPhonicsChecker } from "../src/js/swedish-phonics-module.js"
 
-const wordsToTry = ['cool', 'citron', 'går', 'gör', 'kul', 'kär', 'skor', 'skör']
 
-/* wordsToTry.forEach((word) => {
-  const phonicsChecker = new SwedishPhonicsChecker(word)
-  const initialPhonicSound = phonicsChecker.returnInitialPhonicSound(word)
-  console.log(`The initial phonic sound for the word ${word} is: '${initialPhonicSound}'.`)
-})*/
+  function testIndividualWords() {
+    const wordsToTry = ['cool', 'citron', 'går', 'gör', 'kul', 'kär', 'skor', 'skör']
 
-const longerSentence = 'En   katt med skor kör en en skön bil !! genom Skärholmen'
+    wordsToTry.forEach((word) => {
+      const phonicsChecker = new SwedishPhonicsChecker(word)
+      const initialPhonicSound = phonicsChecker.returnInitialPhonicSound(word)
+      console.log(`The initial phonic sound for the word ${word} is: '${initialPhonicSound}'.`)
+    })
+  }
 
-const sentencePhonicsChecker = new SwedishPhonicsChecker(longerSentence)
-const allIdentifiedWords = sentencePhonicsChecker.returnAllWordsInString()
+  function testSentence() {
+    const longerSentence = 'En katt med skor kör en en skön bil genom Skärholmen'
 
-if (allIdentifiedWords.length > 1) {
-  allIdentifiedWords.forEach((word) => {
-    const phonicsChecker = new SwedishPhonicsChecker(word)
-  const initialPhonicSound = phonicsChecker.returnInitialPhonicSound()
-  const phoneticSpelling = phonicsChecker.phoneticSpelling()
-  console.log(`The initial phonic sound for the word ${word} is: '${initialPhonicSound}' and is pronounced ${phoneticSpelling}.`)
-  })
-}
+    const sentencePhonicsChecker = new SwedishPhonicsChecker(longerSentence)
+    const allIdentifiedWords = sentencePhonicsChecker.returnAllWordsInString()
 
+    if (allIdentifiedWords.length > 1) {
+      allIdentifiedWords.forEach((word) => {
+        const phonicsChecker = new SwedishPhonicsChecker(word)
+        const initialPhonicSound = phonicsChecker.returnInitialPhonicSound()
+        const phoneticSpelling = phonicsChecker.phoneticSpelling()
+        console.log(`The initial phonic sound for the word ${word} is: '${initialPhonicSound}' and is pronounced ${phoneticSpelling}.`)
+      })
+    }
+  }
 
+  function testUnnecessarySpaces() {
+    const testSentence = 'En                    kort         men        skön              promenad'
+    console.log('Original sentence: ' + testSentence)
+
+    const sentencePhonicsChecker = new SwedishPhonicsChecker(testSentence)
+    const allIdentifiedWords = sentencePhonicsChecker.returnAllWordsInString()
+
+    let reconstructedSentence = []
+
+    if (allIdentifiedWords.length > 1) {
+      allIdentifiedWords.forEach((word) => {
+        const phonicsChecker = new SwedishPhonicsChecker(word)
+        const phoneticSpelling = phonicsChecker.phoneticSpelling()
+        reconstructedSentence.push(phoneticSpelling)
+      })
+    }
+    console.log('The modified sentence: ' + reconstructedSentence.toString().replaceAll(',', ' '))
+
+  }
+
+testIndividualWords()
+testSentence()
+testUnnecessarySpaces()
