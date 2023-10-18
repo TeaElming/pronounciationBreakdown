3ed809cd9a35d950b33dbc94050c7bec7645f0ce
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2023-10-18 18:03:46 +0200

    Functioning vowel component

--- src/App/app-components/vowel-component/vowel-component.js
+++ src/App/app-components/vowel-component/vowel-component.js
@@ -1,7 +1,13 @@
-import { SwedishPhonicsChecker } from "../../../js/Module/swedish-phonics-module.js"
+import { SwedishPhonicsChecker } from '../../../js/Module/swedish-phonics-module.js'
 
 const template1 = document.createElement('template')
 template1.innerHTML = `
+<style>
+#vowelSound {
+  display: flex;            /* Use flexbox */
+  align-items: center;      /* Vertically align items in the center */
+}
+</style>
 <div>
   <h1>Vowel Checker</h1>
   <div id="vowelSound">
@@ -10,4 +16,68 @@ template1.innerHTML = `
   </div>
 </div> `
 
-// In the vowelSOund, each vowel shouljd be turned into a button
+/**
+ * This is a custom element that checks the vowel sound of a Swedish word.
+ */
+export class VowelComponent extends HTMLElement {
+  /**
+   * Creates an instance of VowelComponent.
+   */
+  constructor () {
+    super()
+
+    this.attachShadow({ mode: 'open' }).appendChild(template1.content.cloneNode(true))
+
+    this.swedishPhonicsChecker = new SwedishPhonicsChecker()
+    this.inputWord = 'rolig'
+
+    this.vowelSoundDiv = this.shadowRoot.querySelector('#vowelSound')
+    this.phoneticSpellingDiv = this.shadowRoot.querySelector('#phoneticSpelling')
+  }
+
+  /**
+   * Sets the vowel to be checked.
+   */
+  connectedCallback () {
+    this.generateVowelButtons()
+    this.generatePhoneticSpelling()
+  }
+
+  /**
+   * Generates buttons for each vowel sound.
+   */
+  generateVowelButtons () {
+    const vowelSounds = this.swedishPhonicsChecker.returnAllVowelSounds(this.inputWord)
+    console.log(vowelSounds)
+
+    // loop through the vowel sounds and create a button for each
+    vowelSounds.forEach(vowel => {
+      const vowelButton = document.createElement('vowel-button')
+      vowelButton.setVowel(vowel)
+      this.vowelSoundDiv.appendChild(vowelButton)
+    })
+  }
+
+  /**
+   * Sets the input word.
+   *
+   * @param {string} word The word to be checked.
+   */
+  setInputWord (word) {
+    this.inputWord = word
+  }
+
+  /**
+   * Generates the phonetic spelling of the word.
+   */
+  generatePhoneticSpelling () {
+    const phoneticSpelling = this.swedishPhonicsChecker.phoneticVowelSpelling(this.inputWord)
+
+    const phoneticSpellingText = document.createElement('p')
+    phoneticSpellingText.innerHTML = phoneticSpelling
+
+    this.phoneticSpellingDiv.appendChild(phoneticSpellingText)
+  }
+}
+
+customElements.define('vowel-component', VowelComponent)
